3ccdcb39ee488290f8f18324973f858faee6b711
Victor Garcia <victor.garcia@gig.com>
2021-01-17 12:30:15 +0100

    WIP

---
+++ src/components/atoms/Loader/loader.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+export function Loader() {
+    return (
+        <Fragment>
+            <div className="ui active centered inline text loader">
+                Loading...
+            </div>
+        </Fragment>
+    );
+};
